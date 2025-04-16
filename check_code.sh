c290f5156fe7c3dcd09dd004d53f1c6622e8e0e3
Thiago Ferreira <tf938383@gmail.com>
2025-04-16 18:08:18 -0300

    chore: adiciona configurações iniciais do projeto com Docker, Django, Celery, PostgreSQL e RabbitMQ

--- check_code.sh
+++ check_code.sh
@@ -1,67 +1,23 @@
 #!/bin/bash
 
-# Para executar este arquivo é necessário que você dê permissão, execute: chmod +x check_code.sh
-# E para iniciar, execute: ./check_code.sh
+# Rodando Flake8
+echo "🔍 Rodando Flake8..."
+flake8 . > flake8_output.txt
 
-while true; do
-    echo ""
-    echo "🔧 Escolha uma opção:"
-    echo "1) Rodar Flake8"
-    echo "2) Rodar Testes"
-    echo "3) Rodar Ambos (Flake8 + Testes)"
-    echo "4) Sair"
-    read -p "Digite o número da opção: " opcao
+if [ $? -ne 0 ]; then
+    echo "❌ Flake8 encontrou problemas:"
+    cat flake8_output.txt
+else
+    echo "✅ Flake8 passou!"
+fi
 
-    case $opcao in
-        1)
-            echo "🔍 Rodando Flake8..."
-            flake8 . > flake8_output.txt
+# Rodando os testes Django
+echo "🧪 Rodando testes Django..."
+python manage.py test > test_output.txt
 
-            if [ $? -ne 0 ]; then
-                echo "❌ Flake8 encontrou problemas:"
-                cat flake8_output.txt
-            else
-                echo "✅ Flake8 passou!"
-            fi
-            ;;
-        2)
-            echo "🧪 Rodando testes Django..."
-            python manage.py test > test_output.txt
-
-            if [ $? -ne 0 ]; then
-                echo "❌ Testes falharam!"
-                cat test_output.txt
-            else
-                echo "✅ Todos os testes passaram!"
-            fi
-            ;;
-        3)
-            echo "🔍 Rodando Flake8..."
-            flake8 . > flake8_output.txt
-
-            if [ $? -ne 0 ]; then
-                echo "❌ Flake8 encontrou problemas:"
-                cat flake8_output.txt
-            else
-                echo "✅ Flake8 passou!"
-            fi
-
-            echo "🧪 Rodando testes Django..."
-            python manage.py test > test_output.txt
-
-            if [ $? -ne 0 ]; then
-                echo "❌ Testes falharam!"
-                cat test_output.txt
-            else
-                echo "✅ Todos os testes passaram!"
-            fi
-            ;;
-        4)
-            echo "👋 Saindo..."
-            break
-            ;;
-        *)
-            echo "❗ Opção inválida. Tente novamente."
-            ;;
-    esac
-done
+if [ $? -ne 0 ]; then
+    echo "❌ Testes falharam!"
+    cat test_output.txt
+else
+    echo "✅ Todos os testes passaram!"
+fi
