3c345411cf50d29a0ee07851b51467f4f7da2225
Daniel Erick <DanielErick.estudante@gmail.com>
2025-09-29 17:53:18 -0300

    fix: recreating django migrations to deploy

--- app/celery.py
+++ app/celery.py
@@ -1,9 +1,9 @@
-import os
-from celery import Celery
+# import os
+# from celery import Celery
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-app = Celery('app')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+# app = Celery('app')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.autodiscover_tasks()
