8e3d787deb61219197fbcfb42e0d0786fea75929
Daniel Erick <DanielErick.estudante@gmail.com>
2025-09-30 15:45:48 -0300

    chore: disable permissions.py to initial test deploy

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -4,13 +4,14 @@ from rest_framework.exceptions import PermissionDenied
 
 class PublicReadAndCSRFCheckPermission(BasePermission):
     def has_permission(self, request, view):
-        if request.method in ('GET', 'HEAD', 'OPTIONS'):
-            return True
-
-        csrf_token_cookie = request.COOKIES.get('XSRF-TOKEN')
-        csrf_token_header = request.headers.get('X-CSRF-Token')
-
-        if not csrf_token_cookie or not csrf_token_header or csrf_token_header != csrf_token_cookie:
-            raise PermissionDenied('CSRF Verification Failed.')
-
         return True
+        # if request.method in ('GET', 'HEAD', 'OPTIONS'):
+        #     return True
+
+        # csrf_token_cookie = request.COOKIES.get('XSRF-TOKEN')
+        # csrf_token_header = request.headers.get('X-CSRF-Token')
+
+        # if not csrf_token_cookie or not csrf_token_header or csrf_token_header != csrf_token_cookie:
+        #     raise PermissionDenied('CSRF Verification Failed.')
+
+        # return True
