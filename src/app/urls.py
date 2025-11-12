66d8bb80cc9a8688f02286e191898157be6f9ead
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-12 19:39:20 -0300

    chore: auth and blog urls

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('authentication.urls')),
+
+    path('api/v1/', include('authentication.urls')),
+    path('api/v1/', include('blog.urls')),
 ]
