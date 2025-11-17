4de6ead07cc557ef8d7af7443235f0646a3e4347
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-17 16:01:30 -0300

    chore: static settings for dev server

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,3 +9,7 @@ urlpatterns = [
     path('api/v1/', include('authentication.urls')),
     path('api/v1/', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
