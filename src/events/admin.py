ed423dc7cf15adcd411afec73efb92a2ff95b586
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-21 20:22:19 -0300

    feat(events): admin

--- src/events/admin.py
+++ src/events/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from events.models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'event_date', 'is_participation', 'created_at')
+    list_filter = ('type', 'is_participation', 'event_date', 'created_at')
+    search_fields = ('title', 'description', 'content', 'type', 'address')
+    ordering = ('-event_date', 'title')
+    readonly_fields = ('created_at', 'updated_at')
+    
+    fieldsets = (
+        ('Informações Básicas', {
+            'fields': ('title', 'type', 'description')
+        }),
+        ('Data e Horário', {
+            'fields': ('event_date', 'event_end_time')
+        }),
+        ('Localização', {
+            'fields': ('address', 'link')
+        }),
+        ('Conteúdo', {
+            'fields': ('content', 'image')
+        }),
+        ('Configurações', {
+            'fields': ('is_participation',)
+        }),
+        ('Metadados', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
