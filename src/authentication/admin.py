0690026fea956669ff3777300e30cf283ae33309
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-21 20:22:15 -0300

    feat(authentication): admin

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from authentication.models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'cpf', 'is_active', 'is_staff', 'created_at')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'created_at')
+    search_fields = ('email', 'username', 'cpf', 'first_name', 'last_name')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at', 'last_login', 'date_joined')
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Informações Pessoais', {'fields': ('first_name', 'last_name', 'cpf')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined', 'created_at', 'updated_at')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'cpf', 'password1', 'password2', 'is_active', 'is_staff'),
+        }),
+    )
