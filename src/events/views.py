9abdaa4b10832478adf6b14bca40f9ad27a35bdd
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-24 02:52:56 -0300

    chore(events): add djangofilterbackend class on filter_backends

--- src/events/views.py
+++ src/events/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter
 from rest_framework.pagination import LimitOffsetPagination
@@ -11,7 +12,7 @@ from events.filters import EventFilter
 class EventsViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventsSerializer
-    filter_backends = (OrderingFilter, )
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_class = EventFilter
     ordering_fields = ('event_date', 'title', 'created_at')
     pagination_class = LimitOffsetPagination
