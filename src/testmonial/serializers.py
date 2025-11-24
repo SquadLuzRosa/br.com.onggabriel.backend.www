9ac2435a6241639b1b69ba82b9bfd30d0f1f02e9
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-24 08:14:17 -0300

    fix(testmonial):  removed "read only" from function model line and added to function fk on depoiment serializer

--- src/testmonial/serializers.py
+++ src/testmonial/serializers.py
@@ -5,13 +5,13 @@ from testmonial.models import Depoiment, Function
 
 class FunctionSerializer(serializers.Serializer):
     class Meta:
-        model = Function(read_only=True)
+        model = Function
         fields = ['id', 'name', 'created_at', 'updated_at']
         read_only_fields = ['created_at', 'updated_at']
 
 
 class DepoimentSerializer(serializers.Serializer):
-    function = FunctionSerializer
+    function = FunctionSerializer(read_only=True)
 
     class Meta:
         model = Depoiment
