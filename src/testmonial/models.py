f5cf0185c22e995d1edfc718555bfe2da718f81d
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-24 08:24:42 -0300

    chore(testmonial): add friendly verbose name for some fields

--- src/testmonial/models.py
+++ src/testmonial/models.py
@@ -7,7 +7,7 @@ from utils.file_utils import depoiment_image_upload_path
 
 class Function(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name='ID')
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Nome')
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de criação')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Data de atualização')
@@ -23,9 +23,9 @@ class Function(models.Model):
 
 class Depoiment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name='ID')
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='Nome')
     function = models.ForeignKey(Function, on_delete=models.PROTECT, related_name='depoiments', verbose_name='Funções')
-    message = models.TextField()
+    message = models.TextField(verbose_name='Mensagem')
     cover_image = models.ImageField(upload_to=depoiment_image_upload_path, blank=True, null=True, verbose_name='Foto do depoimento')
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de criação')
