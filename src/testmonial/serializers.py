beec159e55cd7161942ecdd1df0d9239b2bf12a3
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-24 07:50:42 -0300

    feat(testmonial): function and depoiment serializers

---
+++ src/testmonial/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from testmonial.models import Depoiment, Function
+
+
+class FunctionSerializer(serializers.Serializer):
+    class Meta:
+        model = Function(read_only=True)
+        fields = ['id', 'name', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
+
+
+class DepoimentSerializer(serializers.Serializer):
+    function = FunctionSerializer
+
+    class Meta:
+        model = Depoiment
+        fields = [
+            'id',
+            'name',
+            'function',
+            'message',
+            'cover_image',
+            'created_at',
+            'updated_at',
+        ]
+        read_only_fields = ['created_at', 'updated_at']
