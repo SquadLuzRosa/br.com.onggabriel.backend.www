e994ef39887e69af39baf80f46a44d013d9a05cc
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-25 08:04:49 -0300

    feat(management): home management models

---
+++ src/management/models/__init__.py
@@ -0,0 +1,23 @@
+from .home import (
+    ContactSection,
+    DepoimentCard,
+    DonateSection,
+    MissionSection,
+    PresentationSection,
+    StatsCard,
+    TributeSection,
+    VolunteerSection,
+)
+from .media import ManagementMedia
+
+__all__ = [
+    'ContactSection',
+    'DepoimentCard',
+    'DonateSection',
+    'ManagementMedia',
+    'MissionSection',
+    'PresentationSection',
+    'StatsCard',
+    'TributeSection',
+    'VolunteerSection',
+]
