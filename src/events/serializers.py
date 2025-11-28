5ec4046e572cee9c5052390be250d309e44a7d38
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-28 13:05:54 -0300

    feat(events serializer): add new medias field on serializer

--- src/events/serializers.py
+++ src/events/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 
 from events.models import Event
+from management.models import ManagementMedia
+from management.serializers import ManagementMediaSerializer
 
 
 class EventsSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(required=False, allow_null=True, use_url=True)
+    medias = ManagementMediaSerializer(many=True, required=False)
+    media_ids = serializers.PrimaryKeyRelatedField(
+        queryset=ManagementMedia.objects.all(),
+        many=True,
+        source='medias',
+        write_only=True,
+        required=False
+    )
 
     class Meta:
         model = Event
@@ -16,11 +25,26 @@ class EventsSerializer(serializers.ModelSerializer):
             'address',
             'description',
             'content',
-            'image',
             'link',
             'event_end_time',
             'is_participation',
             'created_at',
             'updated_at',
+            'medias',
+            'media_ids'
         ]
     read_only_fields = ['created_at', 'updated_at']
+
+    def create(self, validated_data):
+        medias = validated_data.pop('medias', [])
+        instance = super().create(validated_data)
+        if medias:
+            instance.medias.set(medias)
+        return instance
+
+    def update(self, instance, validated_data):
+        medias = validated_data.pop('medias', None)
+        instance = super().update(instance, validated_data)
+        if medias is not None:
+            instance.medias.set(medias)
+        return instance
