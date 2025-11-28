20036bd320de6b772b45522acd62fd5e93d3161e
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-28 13:35:11 -0300

    feat(events urls): add event type and address endpoints

--- src/events/urls.py
+++ src/events/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from events.views import EventsViewSet
+from events.views import AddressModelViewSet, EventsViewSet, EventTypeModelViewSet
 
 router = DefaultRouter()
-router.register(r'event', EventsViewSet, basename='event')
+router.register(r'event', AddressModelViewSet, basename='event')
+router.register(r'event-type', EventTypeModelViewSet, basename='event-type')
+router.register(r'address', EventsViewSet, basename='address')
 
 urlpatterns = [
     path('', include(router.urls))
