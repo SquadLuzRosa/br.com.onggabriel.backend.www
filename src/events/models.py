5d34f86793e125a8b79f8dbd6f4bc2d1037fdf01
Kaironn <jonathas_oliveira17@hotmail.com>
2025-11-28 13:05:24 -0300

    feat(models): add media management on events and allow many

--- src/events/models.py
+++ src/events/models.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 
 from django.db import models
 
-from utils.file_utils import event_image_upload_path
+from management.models import ManagementMedia
 
 
 class Event(models.Model):
@@ -15,7 +15,7 @@ class Event(models.Model):
     link = models.URLField(max_length=200, verbose_name='Link', null=True, blank=True)
     description = models.CharField(max_length=300, verbose_name='Descrição Curta')
     content = models.TextField(blank=True, null=True, verbose_name='Conteúdo do Evento')
-    image = models.ImageField(upload_to=event_image_upload_path, blank=True, null=True, verbose_name='imagem do evento')
+    medias = models.ManyToManyField(ManagementMedia, blank=True, related_name='events', verbose_name='imagens do evento')
     is_participation = models.BooleanField(verbose_name='é obrigatório participação?', default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
