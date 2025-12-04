04743db1f7375d2ed9bb967fba60be1f7d571c90
kaironn <jonathas_oliveira17@hotmail.com>
2025-12-04 06:36:39 -0300

    refactor(event serializer): enables creation via nested medias or media_ids

--- src/events/serializers.py
+++ src/events/serializers.py
@@ -8,7 +8,19 @@ from management.serializers import ManagementMediaSerializer
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ['id', 'name', 'street', 'number', 'district', 'city', 'state', 'zipcode', 'google_maps_url', 'created_at', 'updated_at']
+        fields = [
+            'id',
+            'name',
+            'street',
+            'number',
+            'district',
+            'city',
+            'state',
+            'zipcode',
+            'google_maps_url',
+            'created_at',
+            'updated_at',
+        ]
         read_only_fields = ['created_at', 'updated_at']
 
 
@@ -28,7 +40,11 @@ class EventTypeSerializer(serializers.ModelSerializer):
             instance = self.instance
 
             if instance:
-                if EventType.objects.filter(name=normalized_name).exclude(pk=instance.pk).exists():
+                if (
+                    EventType.objects.filter(name=normalized_name)
+                    .exclude(pk=instance.pk)
+                    .exists()
+                ):
                     raise serializers.ValidationError({
                         'errors': f'Já existe um tipo de evento com o nome "{name}".'
                     })
@@ -40,44 +56,60 @@ class EventTypeSerializer(serializers.ModelSerializer):
         return attrs
 
 
-class EventsSerializer(serializers.ModelSerializer):
-    type = EventTypeSerializer(read_only=True)
-    type_id = serializers.PrimaryKeyRelatedField(
-        queryset=EventType.objects.all(),
-        source='type',
-        write_only=True
-    )
+class EventMediaRelationSerializer(serializers.ModelSerializer):
+    media = ManagementMediaSerializer(read_only=True)
 
+    class Meta:
+        model = EventMediaRelation
+        fields = ['id', 'media', 'is_cover', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
+
+
+class EventMediaWriteSerializer(serializers.Serializer):
+    """Serializer para criar mídia nested no evento"""
+
+    file = serializers.FileField()
+    media_type = serializers.ChoiceField(
+        choices=ManagementMedia.MEDIA_TYPES, default='image'
+    )
+    alt_text = serializers.CharField(max_length=255, required=False, allow_blank=True)
+    title = serializers.CharField(max_length=255, required=False, allow_blank=True)
+    caption = serializers.CharField(max_length=255, required=False, allow_blank=True)
+    is_cover = serializers.BooleanField(default=False, required=False)
+
+
+class EventsSerializer(serializers.ModelSerializer):
+    # Read fields
+    type = EventTypeSerializer(read_only=True)
     address = AddressSerializer(read_only=True)
+    cover_media = serializers.SerializerMethodField()
+    medias = serializers.SerializerMethodField()
+
+    # Write fields
+    type_id = serializers.PrimaryKeyRelatedField(
+        queryset=EventType.objects.all(), source='type', write_only=True
+    )
     address_id = serializers.PrimaryKeyRelatedField(
         queryset=Address.objects.all(),
         source='address',
         write_only=True,
         required=False,
-        allow_null=True
+        allow_null=True,
     )
 
-    medias = serializers.SerializerMethodField()
+    # first option: existent medias
     media_ids = serializers.PrimaryKeyRelatedField(
         queryset=ManagementMedia.objects.all(),
         many=True,
-        source='medias',
         write_only=True,
-        required=False
+        required=False,
     )
-
-    cover_media = serializers.SerializerMethodField()
-
-    def get_cover_media(self, obj):
-        cover = obj.media_links.filter(is_cover=True).first()
-        if not cover:
-            return None
-        return ManagementMediaSerializer(cover.media).data
-
-    def get_medias(self, obj):
-        medias = obj.media_links.filter(is_cover=False).values_list('media', flat=True)
-        queryset = ManagementMedia.objects.filter(pk__in=medias)
-        return ManagementMediaSerializer(queryset, many=True).data
+    cover_media_id = serializers.PrimaryKeyRelatedField(
+        queryset=ManagementMedia.objects.all(),
+        write_only=True,
+        required=False,
+        allow_null=True,
+    )
 
     class Meta:
         model = Event
@@ -96,43 +128,199 @@ class EventsSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'cover_media',
+            'cover_media_id',
             'medias',
-            'media_ids'
+            'media_ids',
         ]
         read_only_fields = ['created_at', 'updated_at']
 
+    def get_cover_media(self, obj):
+        request = self.context.get('request')
+        cover = obj.media_links.filter(is_cover=True).first()
+        if not cover:
+            return None
+        return ManagementMediaSerializer(cover.media, context={'request': request}).data
+
+    def get_medias(self, obj):
+        request = self.context.get('request')
+        media_links = obj.media_links.filter(is_cover=False)
+        medias = [link.media for link in media_links]
+        return ManagementMediaSerializer(
+            medias, many=True, context={'request': request}
+        ).data
+
+    def _parse_nested_medias(self, request):
+        """
+        Parse nested medias of request multipart/form-data.
+        """
+        medias_data = []
+
+        if not request:
+            return medias_data
+
+        index = set()
+        for key in list(request.data.keys()) + list(request.FILES.keys()):
+            if key.startswith('medias['):
+                try:
+                    idx = int(key.split('[')[1].split(']')[0])
+                    index.add(idx)
+                except ValueError, IndexError:
+                    continue
+
+        for idx in sorted(index):
+            media_data = {}
+
+            for field in ['media_type', 'alt_text', 'title', 'caption', 'is_cover']:
+                key = f'medias[{idx}][{field}]'
+                if key in request.data:
+                    value = request.data.get(key)
+                    if field == 'is_cover':
+                        values = ['true', 'True', '1', True]
+                        media_data[field] = value in values
+                    else:
+                        media_data[field] = value
+
+            file_key = f'medias[{idx}][file]'
+            if file_key in request.FILES:
+                media_data['file'] = request.FILES.get(file_key)
+
+            if media_data.get('file'):
+                medias_data.append(media_data)
+
+        return medias_data
+
+    def validate(self, attrs):
+        media_ids = attrs.get('media_ids', [])
+        cover_media_id = attrs.get('cover_media_id')
+
+        if cover_media_id and media_ids and cover_media_id not in media_ids:
+            raise serializers.ValidationError({
+                'cover_media_id': 'A mídia de capa deve estar na lista de mídias do evento.'
+            })
+
+        return attrs
+
     def create(self, validated_data):
-        nested_medias = validated_data.pop('medias', [])
+        media_ids = validated_data.pop('media_ids', [])
+        cover_media_id = validated_data.pop('cover_media_id', None)
+
+        # parse nested medias on request
+        request = self.context.get('request')
+        nested_medias = self._parse_nested_medias(request)
+
         instance = super().create(validated_data)
 
-        first = True
+        # process nested medias
+        created_medias = []
+        cover_from_nested = None
 
         for media_data in nested_medias:
-            media = ManagementMedia.objects.create(**media_data)
-            EventMediaRelation.objects.create(
-                event=instance,
-                media=media,
-                is_cover=first
+            is_cover = media_data.pop('is_cover', False)
+            media_serializer = EventMediaWriteSerializer(data=media_data)
+            media_serializer.is_valid(raise_exception=True)
+
+            # management media creation
+            management_media = ManagementMedia.objects.create(
+                file=media_data.get('file'),
+                media_type=media_data.get('media_type', 'image'),
+                alt_text=media_data.get('alt_text', ''),
+                title=media_data.get('title', ''),
+                caption=media_data.get('caption', ''),
             )
-            first = False
+            created_medias.append(management_media)
+
+            if is_cover:
+                cover_from_nested = management_media
+
+        # if cover has not defined, select the first as cover
+        if created_medias and not cover_from_nested:
+            cover_from_nested = created_medias[0]
+
+        # create EventMediaRelation for nested medias
+        for media in created_medias:
+            EventMediaRelation.objects.create(
+                event=instance, media=media, is_cover=(media == cover_from_nested)
+            )
+
+        # process media_ids
+        if media_ids:
+            # if cover has not defined, select the first as cover
+            if not cover_media_id:
+                cover_media_id = media_ids[0]
+
+            for media in media_ids:
+                # just select as cover if this event hasnt cover
+                is_cover = (media == cover_media_id) and not cover_from_nested
+                EventMediaRelation.objects.create(
+                    event=instance, media=media, is_cover=is_cover
+                )
 
         return instance
 
     def update(self, instance, validated_data):
-        nested_medias = validated_data.pop('medias', None)
+        media_ids = validated_data.pop('media_ids', None)
+        cover_media_id = validated_data.pop('cover_media_id', None)
+
+        # parse nested medias on request
+        request = self.context.get('request')
+        nested_medias = self._parse_nested_medias(request)
+
         instance = super().update(instance, validated_data)
 
-        if nested_medias is not None:
+        # if has new medias on nested or media ids, process
+        if nested_medias or media_ids is not None:
+            # remove old relations
             EventMediaRelation.objects.filter(event=instance).delete()
 
-            first = True
+            # process nested medias (create new)
+            created_medias = []
+            cover_from_nested = None
+
             for media_data in nested_medias:
-                media = ManagementMedia.objects.create(**media_data)
-                EventMediaRelation.objects.create(
-                    event=instance,
-                    media=media,
-                    is_cover=first
+                is_cover = media_data.pop('is_cover', False)
+
+                # Create ManagementMedia
+                management_media = ManagementMedia.objects.create(
+                    file=media_data.get('file'),
+                    media_type=media_data.get('media_type', 'image'),
+                    alt_text=media_data.get('alt_text', ''),
+                    title=media_data.get('title', ''),
+                    caption=media_data.get('caption', ''),
                 )
-                first = False
+                created_medias.append(management_media)
+
+                if is_cover:
+                    cover_from_nested = management_media
+
+            # if cover has not defined, select the first as cover
+            if created_medias and not cover_from_nested:
+                cover_from_nested = created_medias[0]
+
+            # create EventMediaRelation for nested medias
+            for media in created_medias:
+                EventMediaRelation.objects.create(
+                    event=instance, media=media, is_cover=(media == cover_from_nested)
+                )
+
+            # process existent media_ids
+            if media_ids:
+                # if cover has not defined, select the first as cover
+                if not cover_media_id and not cover_from_nested:
+                    cover_media_id = media_ids[0]
+
+                for media in media_ids:
+                    is_cover = (media == cover_media_id) and not cover_from_nested
+                    EventMediaRelation.objects.create(
+                        event=instance, media=media, is_cover=is_cover
+                    )
+
+        elif cover_media_id is not None:
+            # just update cover
+            EventMediaRelation.objects.filter(event=instance, is_cover=True).update(
+                is_cover=False
+            )
+            EventMediaRelation.objects.filter(
+                event=instance, media=cover_media_id
+            ).update(is_cover=True)
 
         return instance
